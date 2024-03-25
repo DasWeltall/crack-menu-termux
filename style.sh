0c17224a8c7d0e898831d952c3af59ad91fd8cfd
DasWeltall <151219570+DasWeltall@users.noreply.github.com>
2024-03-25 10:24:10 +0100

    Update style.sh

--- style.sh
+++ style.sh
@@ -3,17 +3,17 @@
 # Funktion zur Anzeige des Hauptmenüs
 show_main_menu() {
     local options=(
-        1 "ASCII-Kunst anzeigen"
-        2 "Ladebalken anzeigen"
-        3 "Erfolgsnachricht anzeigen"
+        1 "Network Mapper scan"
+        2 "Aircrack-ng"
+        3 "Wireshark"
         4 "Beenden"
     )
 
     local choice
-    choice=$(dialog --clear --title "Cool Sales Script" --menu "Wähle eine Option:" 15 50 4 "${options[@]}" 2>&1 >/dev/tty)
+    choice=$(dialog --clear --title "Cracker Area" --menu "Wähle eine Option:" 15 50 4 "${options[@]}" 2>&1 >/dev/tty)
 
     case $choice in
-        1) show_ascii_art ;;
+        1) run_nmap_scan ;;
         2) show_loading_bar ;;
         3) show_success_message ;;
         4) exit ;;
@@ -27,14 +27,20 @@ show_ascii_art() {
 
 # Funktion zur Anzeige des Ladebalkens
 show_loading_bar() {
-    dialog --clear --msgbox "Hier könnte dein Ladebalken sein!" 10 50
+    dialog --clear --msgbox "wlan0 need root!" 10 50
 }
 
 # Funktion zur Anzeige einer Erfolgsnachricht
 show_success_message() {
-    dialog --clear --msgbox "Erfolgreich! Das coole Skript wurde ausgeführt!" 10 50
+    dialog --clear --msgbox "wlan0 need root" 10 50
 }
+# Funktion zum Ausführen eines Nmap-Scans
+run_nmap_scan() {
+    local output
+    output=$(nmap -sn 192.168.0.0/24 2>&1)
 
+    dialog --clear --title "Nmap Scan Ergebnisse" --msgbox "$output" 20 70
+}
 # Hauptfunktion
 main() {
     while true; do
