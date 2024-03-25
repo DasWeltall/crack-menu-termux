936ab7e96a3f344ab2af227f3027cb7faf35ee82
DasWeltall <151219570+DasWeltall@users.noreply.github.com>
2024-03-25 10:07:07 +0100

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
+# Funktion zur Anzeige des Hauptmenüs
+show_main_menu() {
+    local options=(
+        1 "ASCII-Kunst anzeigen"
+        2 "Ladebalken anzeigen"
+        3 "Erfolgsnachricht anzeigen"
+        4 "Beenden"
+    )
+
+    local choice
+    choice=$(dialog --clear --title "Cool Sales Script" --menu "Wähle eine Option:" 15 50 4 "${options[@]}" 2>&1 >/dev/tty)
+
+    case $choice in
+        1) show_ascii_art ;;
+        2) show_loading_bar ;;
+        3) show_success_message ;;
+        4) exit ;;
+    esac
+}
+
+# Funktion zur Anzeige von ASCII-Kunst
+show_ascii_art() {
+    dialog --clear --msgbox "Hier könnte deine ASCII-Kunst sein!" 10 50
+}
+
 # Funktion zur Anzeige des Ladebalkens
 show_loading_bar() {
-    local delay=0.1
-    local chars="/-\|"
-    local i=0
-
-    while true; do
-        echo -en "\r[${chars:$i:1}] "
-        sleep $delay
-        ((i = (i + 1) % 4))
-    done
+    dialog --clear --msgbox "Hier könnte dein Ladebalken sein!" 10 50
 }
 
 # Funktion zur Anzeige einer Erfolgsnachricht
 show_success_message() {
-    echo -e "\n\nErfolgreich! Das coole Shell-Skript wurde ausgeführt!"
-}
-
-# Funktion zum Ändern des Hintergrundstils von Termux
-change_termux_style() {
-    termux-setup-storage
-    echo -e "\033]11;#000000\007"
-    echo -e "\033]10;#ffffff\007"
+    dialog --clear --msgbox "Erfolgreich! Das coole Skript wurde ausgeführt!" 10 50
 }
 
 # Hauptfunktion
 main() {
-    change_termux_style
-    show_loading_bar &
-    loading_pid=$!
-
-    # Hier kannst du deine eigenen Befehle oder Aktionen einfügen, die du ausführen möchtest
-    sleep 5
-
-    # Töte den Ladebalkenprozess
-    kill $loading_pid > /dev/null 2>&1
-
-    # Lösche die letzte Zeile des Ladebalkens
-    echo -en "\r\033[K"
-
-    # Zeige die Erfolgsnachricht
-    show_success_message
+    while true; do
+        show_main_menu
+    done
 }
 
 # Hauptprogramm starten
